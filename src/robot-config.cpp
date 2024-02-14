d0fe84b2fdb0beb875fec27cec62cbb30c0a27bc
giovanni214 <63879965+giovanni214@users.noreply.github.com>
2024-02-14 01:09:55 -0600

    Update robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,18 +8,25 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor top_right_motor = motor(PORT20, ratio6_1, false);
-motor top_left_motor = motor(PORT11, ratio6_1, true);
-motor right_bottom_motor = motor(PORT16, ratio6_1, false);
-motor right_top_motor = motor(PORT19, ratio6_1, false);
-motor left_bottom_motor = motor(PORT12, ratio6_1, true);
-motor left_top_motor = motor(PORT15, ratio6_1, true);
+motor top_right_motor = motor(PORT5, ratio6_1, true);
+motor top_left_motor = motor(PORT15, ratio6_1, false);
+
+motor right_bottom_motor = motor(PORT10, ratio6_1, false);
+motor right_top_motor = motor(PORT9, ratio6_1, false);
+
+motor left_bottom_motor = motor(PORT20, ratio6_1, true);
+motor left_top_motor = motor(PORT19, ratio6_1, true);
+
+motor left_cata_motor = motor(PORT12, ratio18_1, false);
+motor right_cata_motor = motor(PORT2, ratio18_1, true);
+
+inertial robot_rotation = inertial(PORT4); //fake port
+// vision kicker_vision_seneor = vision(PORTiskyetttt, 255);
+
 controller Controller1 = controller(primary);
-motor left_cata_motor = motor(PORT14, ratio18_1, true);
-motor right_cata_motor = motor(PORT17, ratio18_1, false);
+
 digital_out left_pneumatic = digital_out(Brain.ThreeWirePort.B);
 digital_out right_pneumatic = digital_out(Brain.ThreeWirePort.C);
-digital_in cata_switch = digital_in(Brain.ThreeWirePort.A);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
